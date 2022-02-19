3775a57d0ea81356b04031c945dd7b91e6da4b0a
Erick <erick1junior@hotmail.com>
2022-02-19 19:27:16 -0300

    Add type string

---
+++ type-string.js
@@ -0,0 +1,21 @@
+const texto1 = 'Hello, world!';
+const password = 'password123!';
+const stringNumber = '34519';
+
+//Concatenation (+)
+
+const quote = 'Meu nome é ';
+const myName = "Erick"
+
+console.log(quote + myName);
+
+const city = 'são paulo';
+const input = 'São Paulo';
+
+console.log(city == input);
+//False
+
+console.log(city == input.toLowerCase());
+//True
+
+

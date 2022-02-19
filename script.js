259a54bef76264523ad126f7b0e74a3407a9a998
Erick <erick1junior@hotmail.com>
2022-02-19 19:26:10 -0300

    Add script

---
+++ script.js
@@ -0,0 +1,33 @@
+//VAR
+/*
+var altura = 5;
+var comprimento = 7;
+area = altura * comprimento;
+var area;
+console.log(area); */
+/*
+let forma = 'retangulo';
+let altura = 10;
+let comprimento = 8;
+let area;
+
+if (forma == 'retangulo'){
+    area = altura * comprimento;
+} else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);*/
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma == 'quadrado'){
+    area = altura * comprimento;
+} else{
+    area = (altura * comprimento)/2;
+}
+
+console.log(area);

9257fb2323d7de78965e6b5b28f72cfa51229d17
Erick <erick1junior@hotmail.com>
2022-02-19 19:25:44 -0300

    Add conversoes

---
+++ conversoes.js
@@ -0,0 +1,20 @@
+// Tipos de dados
+// Booleanos
+
+// Conversão Implícita
+const number = 456;
+const strNum = '456';
+
+console.log(number === strNum);
+//false
+console.log(number == strNum);
+//true
+console.log(number + strNum);
+//concatenation '456456'
+
+//Number()
+//String()
+console.log(number + Number(strNum));
+//Number = 912
+
+//Conversão Explícita

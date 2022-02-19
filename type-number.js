e5ac93ba682adcab78a311efe14678aa071cf3bf
Erick <erick1junior@hotmail.com>
2022-02-19 19:26:54 -0300

    Add type-number

---
+++ type-number.js
@@ -0,0 +1,23 @@
+//Type Number
+const myNumber = 3;
+
+const firstNumber = 1;
+const secondNumber = 2;
+
+const operationSum = firstNumber + secondNumber;
+console.log(operationSum)
+
+
+//Float Number
+
+const floatNumber1 = 3.3;
+const floatNumber2 = .5;
+
+const newOperation = floatNumber1 - floatNumber2;
+console.log(newOperation);
+
+// NaN -> Not A Number
+
+const myName = "Erick";
+console.log(myName * firstNumber)
+//NaN

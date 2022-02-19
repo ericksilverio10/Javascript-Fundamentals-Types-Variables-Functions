6994fa4f65553b32c649a1a7276f6c845d2736be
Erick <erick1junior@hotmail.com>
2022-02-19 19:27:41 -0300

    Add type boolean

---
+++ type-boolean.js
@@ -0,0 +1,21 @@
+//boolean
+//true -> verdadeiro
+//false -> falso
+
+const num1 = 1;
+const num2 = 5;
+
+console.log(num1 != num2);
+//True
+
+console.log(num1 == num2);
+//False
+
+const text1 = "alura";
+const text2 = "erick";
+
+console.log(text1 == text2);
+//False
+
+console.log(text1.length == text2.length)
+//True

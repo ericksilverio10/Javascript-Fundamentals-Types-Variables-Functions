1900e2b00454415543a099422e3f4a694472c145
Erick <erick1junior@hotmail.com>
2022-02-19 20:33:15 -0300

    Add script2 file

--- script-2.js
+++ script-2.js
@@ -1,8 +1,9 @@
 // Tipagem dinâmica
 // Untyped
-// => Não precisa declarar o tipo da variável!
+// => Não precisa declarar o tipo da variável
 
 let minhaVar = 577;
 minhaVar = "Texto";
 minhaVar = true;
+
 console.log(minhaVar);

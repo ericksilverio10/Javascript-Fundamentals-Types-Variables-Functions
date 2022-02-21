2b59cbf06bfcd8d29edba0953de3620127f1ae4e
Erick <erick1junior@hotmail.com>
2022-02-21 14:15:19 -0300

    Add functions file

---
+++ functions.js
@@ -0,0 +1,19 @@
+// let x = "";
+// console.log(x);
+// x = "Hi";
+
+//DECLARAÇÃO DE FUNÇÃO
+
+// 1) Declara a função
+function print(texto) {
+    return console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+print("Hello, World!");
+
+function soma(){
+    return 2 + 2;
+}
+console.log(soma());
+

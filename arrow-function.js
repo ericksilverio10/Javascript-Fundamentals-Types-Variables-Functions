6ea21f592e6fc90707b940d9d8478242fa9e2772
Erick <erick1junior@hotmail.com>
2022-02-21 20:35:42 -0300

    Add arrow function file

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+//BASIC FUNCTION
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(10,5))
+//15
+
+function hello(name) {
+    return `Hello, my name is ${name}!`;
+}
+console.log(hello('Erick'));
+//Hello, my name is Erick!
+
+//ARROW FUNCTION
+const arrowSum = (a, b) => a + b;
+console.log(arrowSum(1,1));
+//2
+
+const arrowHello = myName => `Hello, my name is ${myName}!`;
+console.log(arrowHello('Anna'));
+//Hello, my name is Anna!
+
+const sumLittleNumbers = (a, b) => {
+    if (a || b > 10){
+        return "Choose a smaller number"
+    } else{
+        return a + b;
+    }
+}

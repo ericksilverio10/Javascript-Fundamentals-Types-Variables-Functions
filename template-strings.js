82add5fffab3b51f0a6145f613c18d15ab78e835
Erick <erick1junior@hotmail.com>
2022-02-21 13:20:36 -0300

    Add template strings file

---
+++ template-strings.js
@@ -0,0 +1,20 @@
+const myName = "Erick";
+const age = 2022 - 2003;
+const city = "Santo André";
+
+// const phrase = "Meu nome é " + myName + ", minha idade é " + age + ", e eu nasci em " + city;
+
+const phrase = `Meu nome é ${myName}, minha idade é ${age} e nasci na cidade de ${city}`;
+
+
+//console.log(phrase);
+
+const nome = "Erick";
+const idade = "18";
+const bebidaMaior = "Cerveja";
+const bebidaMenor = "Suco";
+                                                       //condição    //true         //false
+const pedido = `${nome} diz: "Por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`;
+console.log(pedido)
+
+

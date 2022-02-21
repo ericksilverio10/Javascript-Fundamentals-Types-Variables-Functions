68efb0c580f7ea12a5eed42835fe2619298b16a6
Erick <erick1junior@hotmail.com>
2022-02-21 13:04:57 -0300

    Add git template string file

---
+++ ternarios.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if (idadeCliente >= idadeMinima){
+//     console.log("Cerveja");
+// } else{
+//     console.log("Suco");
+// }
+            //Condição                    //true    //false
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco")

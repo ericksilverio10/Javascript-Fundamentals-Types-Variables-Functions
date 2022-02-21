ae04a6285b81409ed410a2908ca38dfdc358c869
Erick <erick1junior@hotmail.com>
2022-02-21 20:17:43 -0300

    Add expressoes funcoes file

---
+++ expressoes-funcoes.js
@@ -0,0 +1,11 @@
+// Declaração de função
+function apresentar(){
+    return "olá"
+}
+console.log(apresentar())
+//olá
+
+// Expressão de função
+const soma = function(a, b) {return a + b}
+console.log(soma(1,1));
+// 2

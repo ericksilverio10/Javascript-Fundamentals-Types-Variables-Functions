0b1dde9b5dc2ee295da03bd100830767d89b5723
Erick <erick1junior@hotmail.com>
2022-02-21 12:42:22 -0300

    Add comparacoes file

---
+++ comparacoes.js
@@ -0,0 +1,22 @@
+ // == (Comparação Implicita)
+
+ const num1 = 10;
+ const str = '10';
+
+ console.log(num1 == str);
+ //True
+
+ console.log(num1===str);
+ //False
+
+ //typeof => descobrir tipo de dado
+
+ console.log(typeof num1);
+ //Number
+ console.log(typeof str);
+ //String
+
+ // == só compara o valor
+ // === compara o valor e o tipo de dado
+
+ // Conversão
